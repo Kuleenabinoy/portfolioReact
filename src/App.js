99c6b7a7fa0053d11de0c535c2d14d398346e3cd
leena <annachandy123@gmail.com>
2022-01-14 21:52:56 +1100

    components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-    return <div className="App"></div>;
+export default function App() {
+    return (
+        <div>
+            <PortfolioContainer />
+        </div>
+    );
 }
-
-export default App;
