ec8934d495741b094dfd931a2bc9221479be9125
leena <annachandy123@gmail.com>
2022-01-16 19:05:06 +1100

    resume added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from "../utils/helpers";
 const styles = {
     contactform: {
         margin: 40,
@@ -12,6 +13,37 @@ const styles = {
 };
 
 export default function Contact() {
+    const [formState, setFormState] = useState({ name: "", email: "", message: "" });
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        if (e.target.name === "email") {
+            const isValid = validateEmail(e.target.value);
+
+            if (!isValid) {
+                setErrorMessage("please enter a valid email");
+            } else {
+                setErrorMessage("");
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage("");
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
     return (
         <div>
             <section className="container">
@@ -20,20 +52,49 @@ export default function Contact() {
                     <label style={styles.formlabel} className="formlabel">
                         Name:
                     </label>
-                    <input type="text" className="formcontol"></input>
+                    <input
+                        type="text"
+                        className="formcontol"
+                        name="name"
+                        defaultValue={name}
+                        onChange={handleChange}
+                    ></input>
                     <br></br>
                     <label style={styles.formlabel} className="formlabel">
                         Email:
                     </label>
+                    <input
+                        type="text"
+                        name="email"
+                        defaultValue={email}
+                        onBlur={handleChange}
+                        className="formcontol"
+                    ></input>
 
-                    <input type="text" className="formcontol"></input>
                     <br />
                     <label style={styles.formlabel} className="formlabel">
                         Message:
                     </label>
-                    <input type="textarea" className="formcontrol" rows="5"></input>
+                    <input
+                        type="textarea"
+                        name="message"
+                        defaultValue={message}
+                        onChange={handleChange}
+                        className="formcontrol"
+                        rows="7"
+                    ></input>
+
                     <br />
-                    <button>Submit</button>
+                    <button type="submit" onSubmit={handleSubmit}>
+                        Submit
+                    </button>
+                    <div>
+                        {errorMessage && (
+                            <div>
+                                <p className="error-text">{errorMessage}</p>
+                            </div>
+                        )}
+                    </div>
                 </form>
             </section>
         </div>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,63 @@
 import React from "react";
-
+const styles = {
+    a: {
+        margin: 30,
+    },
+};
 export default function Resume() {
     return (
         <div>
-            <p>Resume</p>
+            <section>
+                <h2 className="top-title">Resume</h2>
+                <div>
+                    <a style={styles.a} href={require("../assets/files/kuleena cv.pdf")}>
+                        Download My Resume{" "}
+                    </a>
+                </div>
+                <div>
+                    <ul>
+                        <br></br>
+                        <h3>Front-End Proficiencies</h3>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>jQuery</li>
+                        <li>Bootstrap</li>
+                        <li>React</li>
+                    </ul>
+                    <ul>
+                        <br></br>
+                        <h3>Back-End Proficiencies</h3>
+                        <li>APIs</li>
+                        <li>NodeJS</li>
+                        <li>Express</li>
+                        <li>Model View Controller (MVC)</li>
+                        <li>REST</li>
+                        <li>Progressive Web Applications (PWA)</li>{" "}
+                    </ul>
+                    <br></br>
+
+                    <ul>
+                        <h3>Dev Tool Proficiencies</h3>
+                        <li>Git</li>
+                        <li>npm</li>
+                        <li>Jest</li>
+                        <li>Webpack</li>
+                    </ul>
+                    <br></br>
+
+                    <ul>
+                        <h3>Database Proficiencies</h3>
+                        <li>MySQL</li>
+                        <li>Sequelize</li>
+                        <li>NoSQL</li>
+                        <li>SQLite</li>
+                        <li>GraphQL</li>
+                        <li>MongoDB</li>
+                        <li>Mongoose</li>
+                    </ul>
+                </div>
+            </section>
         </div>
     );
 }
