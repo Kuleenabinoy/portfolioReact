5fae69d6c4fe1b4f4bd3b80cb0ac6eae8674dd59
leena <annachandy123@gmail.com>
2022-01-16 12:58:39 +1100

    aboutpage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,31 @@
 import React from "react";
-
+const styles = {
+    logo: {
+        width: 50,
+        height: 50,
+        margin: 50,
+        cursor: "pointer",
+    },
+};
 export default function Footer() {
     return (
         <div>
             <div>
                 <a href="https://github.com/Kuleenabinoy">
-                    <img src={require("./assets/logos/github-logo.png")} alt="github" className="logo"></img>
+                    <img
+                        style={styles.logo}
+                        src={require("./assets/logos/github-logo.png")}
+                        alt="github"
+                        className="logo"
+                    ></img>
                 </a>
                 <a href="https://www.linkedin.com/in/kuleenabinoy/">
-                    <img src={require("./assets/logos/linkedin-logo.png")} alt="github" className="logo"></img>
+                    <img
+                        style={styles.logo}
+                        src={require("./assets/logos/linkedin-logo.png")}
+                        alt="github"
+                        className="logo"
+                    ></img>
                 </a>
             </div>
         </div>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,36 @@
 import React from "react";
-
+const styles = {
+    avatar: {
+        height: 300,
+    },
+};
 export default function About() {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>
-                Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris, ut interdum
-                nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non lectus
-                tincidunt fermentum. Suspendisse aliquet orci porta quam semper imperdiet. Praesent euismod mi justo,
-                faucibus scelerisque risus cursus in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-            </p>
-        </div>
+        <section className="container">
+            <h2 class="top-title">Kuleena Binoy</h2>
+            <hr></hr>
+            <div>
+                <img style={styles.avatar} id="avatar" src={require("../assets/images/imageme.jpg")} alt="avatar" />
+                <p>
+                    Web Developer passionate about building and designing creative, impactful and user-friendly web
+                    applications. I am a Graduate in Computer Science, recently earned Certificate in Full Stack Web
+                    Development from the Monash University, with skills in JavaScript, Html, CSS, JavaScript, Node.js,
+                    React.js, and Responsive web design.
+                </p>
+                I always provide result driven solutions utilising my hands-on experience of designing, developing, and
+                implementing applications and solutions with a range of technologies and programming languages.
+                <p>
+                    My work experience in various sectors has strengthened my confidence and increased my resilience to
+                    handle work-related pressures in demanding circumstances. I have worked closely with the management team
+                    of a growing business consulting practice in its early stages and as a team we have achieved all the
+                    team targets.
+                </p>
+                <p>
+                    I am looking forward to implementing my technical skills and collaborative nature to write cleaner code,
+                    identify and fix errors, and assist my team in achieving the performance targets, in line with the
+                    organisational objectives and values.
+                </p>
+            </div>
+        </section>
     );
 }
